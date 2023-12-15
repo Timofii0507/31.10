fb261320aeee9081f9b17dc1ebbfdbef520e993a
Timofii0507 <timofiiyurkov@gamil.com>
2023-12-15 19:46:13 +0200

    Друге завдання  з  дз

---
+++ lab2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void reverse(T* arr, size_t size)
+{
+    for (size_t i = 0; i < size / 2; i++)
+    {
+        T temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    reverse(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
